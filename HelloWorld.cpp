1517b5430ff00d4344c1f22a7acf9b37103adf30
ArunThirunavukkarasu <arunthirunavukkarasu96@gmail.com>
2020-04-20 12:07:00 +0530

    Hello World Program

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+main(){
+
+    cout<<"Hello World"<<endl;
+    string name;
+    cin>>name;
+    cout<<"the name is: "<<name<<endl;
+//    return 0;
+}
---
+++ HelloWorld_2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+// using namespace std;
+
+int main(){
+    std::cout<<"Hello World"<< std::endl;
+    int a = 0;
+    if (a != 0)
+    {
+        std::cout<<("a: ")<<a<<std::endl;
+    }
+    else
+    {
+        std::cout<<"ValueError"<<std::endl;
+    }
+    
+    
+    return 0;
+}
