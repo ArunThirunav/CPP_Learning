70d3b125f0e43cded065c90ae7f4941e4fa3a0c9
arunt <arunt@vtitan.com>
2020-06-08 18:23:13 +0530

    added rough

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*
+This code is the beginning of learning cpp
+*/
+
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+// using namespsace std;
+
+int main(){
+
+    std::cout<<"Hello World"<< std::endl;   
+    std::cout<<"Hello World"<< std::endl;   
+    return 0;
+}
